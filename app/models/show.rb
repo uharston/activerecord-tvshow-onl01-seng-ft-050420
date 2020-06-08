b675f1311994967385caf4de2d834bffc96727a8
Uriah Harston <uharston@gmail.com>
2020-06-08 21:48:34 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -22,14 +22,14 @@ class Show < ActiveRecord::Base
     Show.sum(:rating)
   end
 
-  def self.popular_shows
-    Show.where(:rating > ? 5)
-
-  end
-
-  # def self.shows_by_alphabetical_order
-  #   Show.order(name:)
+  # def self.popular_shows
+  #   Show.where(:rating > ? 5)
+  # 
   # end
+
+  def self.shows_by_alphabetical_order
+    Show.order(name:)
+  end
 
 
 
