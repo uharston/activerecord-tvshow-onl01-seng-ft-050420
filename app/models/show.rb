4527d4d147c9211b68940258783b080f5ad87166
Uriah Harston <uharston@gmail.com>
2020-06-08 21:18:35 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,44 +1,44 @@
-class Show < ActiveRecord::Base
-
-  def highest_rating
-
-  end
-
-  def most_popular_show
-
-  end
-
-  def lowest_rating
-
-  end
-
-  def least_popular_show
-
-  end
-
-  def popular_shows
-
-  end
-
-  def shows_by_alphabetical_order
-
-  end
-
-
-
-end
-
-class CreateShows < ActiveRecord::Migration[5.2]
-  def change
-    create_table :shows do |s|
-      s.string :name
-      s.string :network
-      s.string :day
-      s.integer :rating
-  end
-end
-
-class AddSeasonsToShows < ActiveRecord::Migration[5.2]
-  def change
-    add_column :shows, :season, :string
-end
+# class Show < ActiveRecord::Base
+# 
+#   def highest_rating
+# 
+#   end
+# 
+#   def most_popular_show
+# 
+#   end
+# 
+#   def lowest_rating
+# 
+#   end
+# 
+#   def least_popular_show
+# 
+#   end
+# 
+#   def popular_shows
+# 
+#   end
+# 
+#   def shows_by_alphabetical_order
+# 
+#   end
+# 
+# 
+# 
+# end
+# 
+# class CreateShows < ActiveRecord::Migration[5.2]
+#   def change
+#     create_table :shows do |s|
+#       s.string :name
+#       s.string :network
+#       s.string :day
+#       s.integer :rating
+#   end
+# end
+# 
+# class AddSeasonsToShows < ActiveRecord::Migration[5.2]
+#   def change
+#     add_column :shows, :season, :string
+# end
