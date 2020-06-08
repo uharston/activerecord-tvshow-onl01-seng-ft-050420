8ff5bdfed400a60081e996bc3d8f918f8fcdcda9
Uriah Harston <uharston@gmail.com>
2020-06-08 21:04:25 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shoes.rb
+++ db/migrate/001_create_shoes.rb
@@ -1,9 +1,9 @@
-class CreateShows < ActiveRecord::Migration 
-  def change 
-    create_table :shows do |s| 
-      s.string :name 
-      s.string :network 
-      s.string :day 
-      s.integer :rating 
-  end 
+class CreateShows < ActiveRecord::Migration
+  def change
+    create_table :shows do |s|
+      s.string :name
+      s.string :network
+      s.string :day
+      s.integer :rating
+  end
 end 
