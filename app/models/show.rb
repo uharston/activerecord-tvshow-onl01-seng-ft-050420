f5b2b02bb73b78d39241f7bd6ea43fb0096d9e2b
Uriah Harston <uharston@gmail.com>
2020-06-08 21:11:50 +0000

    Automatically backed up by Learn

---
+++ app/models/show.rb
@@ -0,0 +1,29 @@
+class Show < ActiveRecord::Base 
+
+  def highest_rating
+
+  end 
+
+  def most_popular_show
+
+  end 
+
+  def lowest_rating
+
+  end 
+
+  def least_popular_show 
+
+  end 
+
+  def popular_shows 
+
+  end 
+
+  def shows_by_alphabetical_order
+
+  end 
+
+  
+
+end 
