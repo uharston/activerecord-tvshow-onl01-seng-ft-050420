fc27027a7b42bfc2942161c2de1c1bd36cc0e6fe
Uriah Harston <uharston@gmail.com>
2020-06-08 21:09:27 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,9 +1,4 @@
 class AddSeasonsToShows < ActiveRecord::Migration[5.2]
   def change
-    create_table :seasons do |s|
-      s.string :name
-      s.string :network
-      s.string :day
-      s.integer :rating
-  end
-end 
+    add_column :shows, :season, :string
+end
